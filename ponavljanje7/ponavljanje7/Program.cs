09adffc23508053e5a1b298d881b0da050a4b65f
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-09-19 07:29:03 +0200

    Add files via upload

---
+++ ponavljanje7/ponavljanje7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Prim = true;
+            int i, j;
+            Console.WriteLine("Primarni brojevi od 1 do 100 su: ");
+            for (i = 2; i <= 100; i++)
+            {
+
+                for (j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        Prim = false;
+                        break;
+                    }
+                }
+
+                if (Prim)
+
+                {
+                    Console.Write("\t" + i);
+                }
+
+                Prim = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
